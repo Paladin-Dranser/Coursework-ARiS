f1b888f903163a32fe3f968f2946891bb2e39288
Paladin-Dranser <anton.butsko@gmail.com>
2019-11-07 13:08:44 +0300

    Add catalog

--- main.py
+++ main.py
@@ -14,5 +14,35 @@ def contact_page():
 def login_page():
     return render_template('login.html')
 
+@app.route("/fantasy")
+def fantasy_page():
+    genre = 'Fantasy'
+    return render_template('catalog.html', genre=genre)
+
+@app.route("/novels")
+def novel_page():
+    genre = 'Novel'
+    return render_template('catalog.html', genre=genre)
+
+@app.route("/tales")
+def tale_page():
+    genre = 'Tale'
+    return render_template('catalog.html', genre=genre)
+
+@app.route("/math")
+def math_page():
+    genre = 'Math'
+    return render_template('catalog.html', genre=genre)
+
+@app.route("/it")
+def it_page():
+    genre = 'IT'
+    return render_template('catalog.html', genre=genre)
+
+@app.route("/web")
+def web_page():
+    genre = 'Web'
+    return render_template('catalog.html', genre=genre)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
