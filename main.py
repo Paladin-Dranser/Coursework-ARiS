6c6636a93be48e96b7933fedda017d25ff8e91bb
Paladin-Dranser <anton.butsko@gmail.com>
2019-12-13 21:15:15 +0300

    Add radio buttons

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_books_by_genre(genre):
     return books_info
 
 
-def search(book_name):
+def search_by_book(book_name):
     book_name = '%' + book_name + '%'
     cursor = mariadb_connection.cursor()
     cursor.execute("SELECT books.name, books.pdf_link, books.image_link, \
@@ -43,6 +43,25 @@ def search(book_name):
     cursor.close()
 
     return books_info
+
+def search_by_author(author):
+    author = '%' + author + '%'
+    cursor = mariadb_connection.cursor()
+
+    cursor.execute("SELECT books.name, books.pdf_link, books.image_link, \
+        authors.name, genres.name \
+        FROM authors \
+        JOIN books_authors ON authors.id = books_authors.author_id \
+        JOIN books ON books_authors.book_id = books.id \
+        JOIN books_genres ON books.id = books_genres.book_id \
+        JOIN genres ON books_genres.genre_id = genres.id \
+        WHERE LOWER(authors.name) LIKE LOWER(%s)", (author,)
+    )
+
+    books_info = cursor.fetchall()
+    cursor.close()
+
+    return books_info
     
 
 app = Flask(__name__)
@@ -50,34 +69,46 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
     if request.method == 'POST':
-        book_name = request.form['search']
-        return redirect(url_for('search_page', book_name=book_name))
+        search_words = request.form['search']
+        radio_button = request.form['options']
+        return redirect(url_for('search_page', search_words=search_words,
+                                radio_button=radio_button))
 
     return render_template('main.html')
 
 @app.route("/search", methods=['GET', 'POST'])
 def search_page():
     if request.method == 'POST':
-        book_name = request.form['search']
+        search_words = request.form['search']
+        radio_button = request.form['options']
     else:
-        book_name = request.args['book_name']
+        search_words = request.args['search_words']
+        radio_button = request.args['radio_button']
 
-    books_info = search(book_name)
+    if radio_button == 'book':
+        books_info = search_by_book(search_words)
+    elif radio_button == 'author':
+        books_info = search_by_author(search_words)
+
     return render_template('catalog.html', books_info=books_info)
 
 @app.route("/contact", methods=['GET', 'POST'])
 def contact_page():
     if request.method == 'POST':
-        book_name = request.form['search']
-        return redirect(url_for('search_page', book_name=book_name))
+        search_words = request.form['search']
+        radio_button = request.form['options']
+        return redirect(url_for('search_page', search_words=search_words,
+                                radio_button=radio_button))
 
     return render_template('contact.html')
 
 @app.route("/fantasy", methods=['GET', 'POST'])
 def fantasy_page():
     if request.method == 'POST':
-        book_name = request.form['search']
-        return redirect(url_for('search_page', book_name=book_name))
+        search_words = request.form['search']
+        radio_button = request.form['options']
+        return redirect(url_for('search_page', search_words=search_words,
+                                radio_button=radio_button))
 
     genre = 'Fantasy'
     books_info = get_books_by_genre(genre)
@@ -86,8 +117,10 @@ def fantasy_page():
 @app.route("/novels", methods=['GET', 'POST'])
 def novel_page():
     if request.method == 'POST':
-        book_name = request.form['search']
-        return redirect(url_for('search_page', book_name=book_name))
+        search_words = request.form['search']
+        radio_button = request.form['options']
+        return redirect(url_for('search_page', search_words=search_words,
+                                radio_button=radio_button))
 
     genre = 'Novels'
     books_info = get_books_by_genre(genre)
@@ -96,8 +129,10 @@ def novel_page():
 @app.route("/tales", methods=['GET', 'POST'])
 def tale_page():
     if request.method == 'POST':
-        book_name = request.form['search']
-        return redirect(url_for('search_page', book_name=book_name))
+        search_words = request.form['search']
+        radio_button = request.form['options']
+        return redirect(url_for('search_page', search_words=search_words,
+                                radio_button=radio_button))
 
     genre = 'Fairy tales'
     books_info = get_books_by_genre(genre)
@@ -106,8 +141,10 @@ def tale_page():
 @app.route("/math", methods=['GET', 'POST'])
 def math_page():
     if request.method == 'POST':
-        book_name = request.form['search']
-        return redirect(url_for('search_page', book_name=book_name))
+        search_words = request.form['search']
+        radio_button = request.form['options']
+        return redirect(url_for('search_page', search_words=search_words,
+                                radio_button=radio_button))
 
     genre = 'Math'
     books_info = get_books_by_genre(genre)
@@ -116,8 +153,10 @@ def math_page():
 @app.route("/it", methods=['GET', 'POST'])
 def it_page():
     if request.method == 'POST':
-        book_name = request.form['search']
-        return redirect(url_for('search_page', book_name=book_name))
+        search_words = request.form['search']
+        radio_button = request.form['options']
+        return redirect(url_for('search_page', search_words=search_words,
+                                radio_button=radio_button))
 
     genre = 'IT'
     books_info = get_books_by_genre(genre)
@@ -126,8 +165,10 @@ def it_page():
 @app.route("/web", methods=['GET', 'POST'])
 def web_page():
     if request.method == 'POST':
-        book_name = request.form['search']
-        return redirect(url_for('search_page', book_name=book_name))
+        search_words = request.form['search']
+        radio_button = request.form['options']
+        return redirect(url_for('search_page', search_words=search_words,
+                                radio_button=radio_button))
 
     genre = 'Web'
     books_info = get_books_by_genre(genre)
